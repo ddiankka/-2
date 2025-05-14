f3ea95ff778f0b8f4daf32ef76018f6f84318c79
ddiankka <diana.pavlenko@e-u.edu.ua>
2025-05-14 20:56:06 +0300

    Update hw08.rs

---
+++ src/homework/hw08.rs
@@ -0,0 +1,39 @@
+// Файл: src/homeworks/hw08.rs
+
+pub fn is_prime(n: u64) -> bool {
+    if n <= 1 {
+        return false;
+    }
+    if n == 2 {
+        return true;
+    }
+    if n % 2 == 0 {
+        return false;
+    }
+
+    let limit = (n as f64).sqrt() as u64;
+    for i in (3..=limit).step_by(2) {
+        if n % i == 0 {
+            return false;
+        }
+    }
+
+    true
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_is_prime_cases() {
+        assert!(!is_prime(0));
+        assert!(!is_prime(1));
+        assert!(is_prime(2));
+        assert!(is_prime(3));
+        assert!(!is_prime(4));
+        assert!(is_prime(5));
+        assert!(!is_prime(9));
+        assert!(is_prime(13));
+    }
+}
